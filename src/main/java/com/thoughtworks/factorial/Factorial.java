ce97be78a15ebc210b02758e9f1e2f66ba550905
louiedp3 <louie.perez3@gmail.com>
2015-01-05 23:55:04 -0900

    shouldThrowIllegalArgument pass

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,7 +2,9 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0) {
+        if (i < 0) {
+            throw new IllegalArgumentException("i must be positive");
+        } else if (i == 0) {
             return 1;
         } else {
             return i * compute(i - 1);
